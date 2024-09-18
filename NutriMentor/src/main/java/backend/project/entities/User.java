92590ed5718cd543f2f98e2e01f37549cb2e9b7c
italo18-User <u20211e474@upc.edu.pe>
2024-09-17 23:16:19 -0500

    FIRSTING 3

---
+++ NutriMentor/src/main/java/backend/project/entities/User.java
@@ -0,0 +1,31 @@
+package backend.project.entities;
+
+
+import ch.qos.logback.core.net.server.Client;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+    private Boolean active;
+
+    @ManyToOne
+    @JoinColumn(name="authority_id")
+    private Authority authority;
+
+    @OneToOne
+    @JoinColumn(name="client_id")
+    private Client client;
+
+}
